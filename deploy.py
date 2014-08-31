ff78a934744dc962a944c82fa68b736cc40b5020
Mario Ribeiro <mario.ribas@gmail.com>
2014-08-31 02:10:17 +0100

    update

--- deploy.py
+++ deploy.py
@@ -5,100 +5,82 @@ from distutils.dir_util import copy_tree, remove_tree
 from subprocess import call
 import argparse
 
+QMAKE_EXE = "/opt/Qt5.1.1/5.1.1/gcc/bin/qmake"
+
 def cp(root_src,root_dest,rel_path):
 	print "Copying %s from %s to %s" % (rel_path, root_src, root_dest)
-	copy_tree(path.join(root_src, rel_path), path.join(root_dest, rel_path))
-	
+	copy_tree(path.join(root_src, rel_path), path.join(root_dest, rel_path))	
 
 def deploy():
 
 	parser = argparse.ArgumentParser()
-
-	parser.add_argument("-A", "--all", action="store_true", help='deploy everything from scratch')
+	parser.add_argument("--emb", action="store_true", help='deploy embedded')
+	parser.add_argument("--toolchain", action="store_true", help='copy toolchain')
+	parser.add_argument("--clean", action="store_true", help='clean')
 	args = parser.parse_args()
 
 	rootdir = getcwd()
 	
-	deploy_embedded = True
-	cp_toolchain = False
+	deploy_embedded = args.emb
+	copy_toolchain = args.toolchain
 
-	# Paths
-	QMAKE_EXE = "/opt/Qt5.1.1/5.1.1/gcc/bin/qmake"
+	call([QMAKE_EXE])
 
-	if args.all:
+	if args.clean:
+		print "! Cleaning"
 		if path.exists("release"):
 			remove_tree("release")
-		deploy_embedded = True
-		cp_toolchain = False
-
-		print " === Build qkide"
-		call([QMAKE_EXE])
+			call(["make","clean"])
+		print "! Building QkIDE"
 		call(["make"])
 
-
-
 	SOFTWARE_DIR = path.join(rootdir, "../")
-	SOFTWARE_SHARED_DIR = path.join(SOFTWARE_DIR, "shared")
-	EMBEDDED_DIR  = path.join(rootdir, "../../embedded")
-	QKPROGRAM_DIR = path.join(EMBEDDED_DIR, "qkprogram")
-	QKPERIPHERAL_DIR = path.join(EMBEDDED_DIR, "qkperipheral")
-	QKDSP_DIR = path.join(EMBEDDED_DIR, "qkdsp")
-	EMBEDDED_SHARED_DIR = path.join(EMBEDDED_DIR, "shared")
+	EMB_DIR  = path.join(rootdir, "../../embedded")
+	EMB_SHARED_DIR = path.join(EMB_DIR, "shared")
 
+	RC_DIR = path.join(rootdir, "resources")
 	TOOLS_DIR = path.join(rootdir, "../tools/linux")
-	CTAGS_DIR = path.join(TOOLS_DIR, "ctags")
 
 	RELEASE_DIR = path.join(rootdir, "release")
-	RELEASE_RESOURCES_DIR = path.join(RELEASE_DIR, "resources")
-	RELEASE_TOOLS_DIR = path.join(RELEASE_RESOURCES_DIR, "tools")
-	RELEASE_EMBEDDED_DIR  = path.join(RELEASE_RESOURCES_DIR, "embedded")
-	RELEASE_QKPROGRAM_DIR = path.join(RELEASE_EMBEDDED_DIR, "qkprogram")
-	RELEASE_QKPERIPHERAL_DIR = path.join(RELEASE_EMBEDDED_DIR, "qkperipheral") 
-	RELEASE_QKDSP_DIR = path.join(RELEASE_EMBEDDED_DIR, "qkdsp") 
+	RELEASE_RC_DIR = path.join(RELEASE_DIR, "resources")
+	RELEASE_TOOLS_DIR = path.join(RELEASE_RC_DIR, "tools")
+	RELEASE_EMB_DIR  = path.join(RELEASE_RC_DIR, "embedded")
 
-	# Build dependencies
 	libs = ["qkcore", "qkwidget"]
 	for lib in libs:
-		print " === Build %s" % (lib)
+		print "! Build %s" % (lib)
 		chdir(path.join(SOFTWARE_DIR, lib))
 		call(QMAKE_EXE)
+#		if args.clean:
+#			call(["make","clean"])
 		call("make")
 		chdir(rootdir)
 
-	if cp_toolchain:
-		print " === Copy embedded toolchain"
-		cp(EMBEDDED_SHARED_DIR, RELEASE_EMBEDDED_DIR, "toolchain/common")
-		cp(EMBEDDED_SHARED_DIR, RELEASE_EMBEDDED_DIR, "toolchain/linux")
+	if copy_toolchain:
+		print "! Copy toolchain"
+		cp(EMB_SHARED_DIR, RELEASE_EMB_DIR, "toolchain/common")
+		cp(EMB_SHARED_DIR, RELEASE_EMB_DIR, "toolchain/linux")
 
 	if deploy_embedded:
-		chdir(EMBEDDED_DIR)
+		print "! Deploy embedded"
+		chdir(EMB_DIR)
 		call(["python", "deploy.py"])
 		chdir(rootdir)
+		print "! Copy embedded"
+		cp(EMB_DIR,  RELEASE_EMB_DIR,  "target")
+		emb_libs = ["qkprogram", "qkperipheral", "qkdsp"]
+		for emb_lib in emb_libs:
+			cp(path.join(EMB_DIR, emb_lib), path.join(RELEASE_EMB_DIR, emb_lib), "doc")
+			cp(path.join(EMB_DIR, emb_lib), path.join(RELEASE_EMB_DIR, emb_lib), "include")
+			cp(path.join(EMB_DIR, emb_lib), path.join(RELEASE_EMB_DIR, emb_lib), "build/lib")
 
-	# Copy files
-	print " === Copy embedded"
-	cp(EMBEDDED_DIR,  RELEASE_EMBEDDED_DIR,  "target")
 
-
-	cp(QKPROGRAM_DIR, RELEASE_QKPROGRAM_DIR, "doc")
-	cp(QKPROGRAM_DIR, RELEASE_QKPROGRAM_DIR, "include")
-	cp(QKPROGRAM_DIR, RELEASE_QKPROGRAM_DIR, "build/lib")
-
-	cp(QKPERIPHERAL_DIR, RELEASE_QKPERIPHERAL_DIR, "doc")
-	cp(QKPERIPHERAL_DIR, RELEASE_QKPERIPHERAL_DIR, "include")
-	cp(QKPERIPHERAL_DIR, RELEASE_QKPERIPHERAL_DIR, "build/lib")
-
-	cp(QKDSP_DIR, RELEASE_QKDSP_DIR, "doc")
-	cp(QKDSP_DIR, RELEASE_QKDSP_DIR, "include")
-	cp(QKDSP_DIR, RELEASE_QKDSP_DIR, "build/lib")
-
-	print " === Copy other resources"
+	print "! Copy other resources"
+	cp(".", RELEASE_DIR, "examples")
+	cp(RC_DIR, RELEASE_RC_DIR, "html")
+	cp(RC_DIR, RELEASE_RC_DIR, "theme")
 	cp(TOOLS_DIR, RELEASE_TOOLS_DIR, "ctags")
-	cp("resources", RELEASE_RESOURCES_DIR, "html")
-	cp("resources", RELEASE_RESOURCES_DIR, "theme")
-	cp("", RELEASE_DIR, "examples")
 
-	print "Done"
 
 
 if __name__ == "__main__":
